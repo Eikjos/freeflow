137bf3be103ae5de457f69838d3725732d07404a
Thomas Hamelin <thomashamelin27@outlook.fr>
2025-05-20 13:14:57 +0200

    fix: remove tasks tab

--- apps/client/src/app/(pages)/(enterprise)/activities/page.tsx
+++ apps/client/src/app/(pages)/(enterprise)/activities/page.tsx
@@ -1,9 +1,7 @@
 import ProjectTab from "@components/templates/project-tab";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@components/ui/tabs";
 import { getTranslations } from "next-intl/server";
 import { headers } from "next/headers";
 import { EnterpriseInfo } from "../../../../types/enterprise-info-type";
-import TasksTab from "@components/templates/task-tab";
 
 export default async function ActivitiesPage() {
   const t = await getTranslations();
@@ -16,18 +14,7 @@ export default async function ActivitiesPage() {
       <h1 className="font-amica text-4xl">
         {t("activities.enterprise", { enterprise: enterprise?.name })}
       </h1>
-      <Tabs defaultValue="projects" className="mt-5">
-        <TabsList>
-          <TabsTrigger value="projects">{t("activities.projects")}</TabsTrigger>
-          <TabsTrigger value="tasks">{t("activities.tasks")}</TabsTrigger>
-        </TabsList>
-        <TabsContent value="projects">
-          <ProjectTab enterpriseId={enterprise?.id ?? 0} />
-        </TabsContent>
-        <TabsContent value="tasks">
-          <TasksTab />
-        </TabsContent>
-      </Tabs>
+      <ProjectTab enterpriseId={enterprise?.id ?? 0} />
     </div>
   );
 }
