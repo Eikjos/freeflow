d32159023c483bc221431f2a68ea0070181b2cd1
Thomas Hamelin <thomashamelin27@outlook.fr>
2025-12-14 16:23:31 +0100

    feat: finish translation tarif page

--- apps/client/src/app/(pages)/tarifs/page.tsx
+++ apps/client/src/app/(pages)/tarifs/page.tsx
@@ -1,18 +1,20 @@
 import Footer from "@components/templates/footer";
 import Header from "@components/templates/header";
 import TarifsSection from "@components/templates/tarifs-section";
+import { getTranslations } from "next-intl/server";
 import Link from "next/link";
 
-export default function TarifsPage() {
+export default async function TarifsPage() {
+  const t = await getTranslations();
   return (
     <div>
       <Header displayMenu={false} />
       <TarifsSection />
       <div className="w-1/2 text-center mx-auto">
-        <p>Déjà inscrit ? {" "}
+        <p>{t("login.alreadySubscribe")} {" "}
           <Link as="span" href="/login">
             <span className="text-primary">
-              Connectez-vous
+              {t("login.loginYou")}
             </span>
           </Link>
         </p>
