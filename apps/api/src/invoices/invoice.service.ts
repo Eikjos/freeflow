ee3a94d9e13515a54b58bcf7feb5c3ce8267633a
Thomas Hamelin <thomashamelin27@outlook.fr>
2026-02-28 14:09:50 +0100

    fix: lint

--- apps/api/src/invoices/invoice.service.ts
+++ apps/api/src/invoices/invoice.service.ts
@@ -6,12 +6,12 @@ import {
 } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { mapCustomerToDetailDto } from 'dtos/customers/customer.dto';
+import { InvoiceDto } from 'dtos/invoices/invoice.dto';
 import {
   CreateInvoiceDto,
   CreateInvoiceLineDto,
 } from 'dtos/invoices/invoice-create.dto';
 import { InvoiceFilterDataDto } from 'dtos/invoices/invoice-filter.dto';
-import { InvoiceDto } from 'dtos/invoices/invoice.dto';
 import QuoteValidateDto from 'dtos/invoices/quote-validate.dto';
 import {
   PaginationFilterDto,
